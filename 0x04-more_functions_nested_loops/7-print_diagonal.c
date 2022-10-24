f324f8a1511e7369aa65416de032e31f3ca9d178
Ziggiphase <bellobasit790@gmail.com>
2022-10-23 22:26:38 -0700

    Mistake

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,29 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* print_diagonal - prints \ alot
-* @n: number of dashes
-*
-* Return: void
+* print_diagonal - draws a diagonal line on the terminal
+* @n: number of times the character \ should be printed
 */
-void print_diagonal(int n)
+void print_diagonal(int n)	
 {
-	int i, spaces;
-
-	if (n > 0)
-	{
-		for (i = 1; i <= n; i++)
-		{
-			for (spaces = 0; spaces < i; spaces++)
-			{
-				_putchar(' ');
-			}
-			_putchar(92);
-			_putchar('\n');
-		}
-	}
-	else
-	_putchar('\n');
+	if (n <= 0)	
+	{	
+		_putchar('\n');	
+	} else		
+	{	
+		int i, j;
+		
+		for (i = 0; i < n; i++)		
+		{		
+			for (j = 0; j < n; j++)			
+			{			
+				if (j == i)				
+					_putchar('\\');			       
+				else if (j < i)
+					
+					_putchar(' ');				
+			}			
+			_putchar('\n');			
+		}		
+	}	
 }
